fc2040e2e22e6c22b2f65085f21eb9dce77ca4b7
eltonv <eltonmvillalta@gmail.com>
2021-12-22 23:50:51 -0800

    -Added Quick Sort -Added computational speed of selected algorithm

--- SortingMain.py
+++ SortingMain.py
@@ -1,3 +1,6 @@
+import time
+
+
 def selection_sort(to_sort, sorted_ind, min_ind):
     sorted_ind = 0
     min_ind = 0
@@ -46,17 +49,44 @@ def merge_sort(to_sort):
 
 
 def insertion_sort(to_sort):
-    for i in range(1, len(to_sort) ):
+    for i in range(1, len(to_sort)):
         j = i
         while j > 0 and to_sort[j - 1] > to_sort[j]:
             to_sort[j], to_sort[j - 1] = to_sort[j - 1], to_sort[j]
             j = j - 1
 
+
 def bubble_sort(to_sort):
     for i in range(1, len(to_sort)):
         for j in range(0, len(to_sort) - 1):
-            if to_sort[j] > to_sort[j+1]:
-                to_sort[j], to_sort[j+1] = to_sort[j+1], to_sort[j]
+            if to_sort[j] > to_sort[j + 1]:
+                to_sort[j], to_sort[j + 1] = to_sort[j + 1], to_sort[j]
+
+
+def qs_partition(start, end, to_Sort):
+    pivot_ind = start
+    pivot = to_Sort[pivot_ind]
+
+    while start < end:
+        while start < len(to_Sort) and to_Sort[start] <= pivot:
+            start += 1
+
+        while to_Sort[end] > pivot:
+            end -= 1
+
+        if start < end:
+            to_Sort[start], to_Sort[end] = to_Sort[end], to_Sort[start]
+    to_Sort[end], to_Sort[pivot_ind] = to_Sort[pivot_ind], to_Sort[end]
+    return end
+
+
+def quick_sort(start, end, to_sort):
+    if start < end:
+        partition_ind = qs_partition(start, end, to_sort)
+
+        quick_sort(start, partition_ind - 1, to_sort)
+        quick_sort(partition_ind + 1, end, to_sort)
+
 
 # main
 number_elem = int(input("Enter the amount of numbers to sort -> "))
@@ -70,35 +100,36 @@ for i in range(0, number_elem):
     element = int(input())
     need_sort.append(element)
 
-sortAlgo = int(input("Choose sorting algorithm (1-#): \n 1. Selection Sort \n 2. Merge Sort \n "
-                     "3. Insertion Sort \n 4. Bubble Sort \n -> "))
+sortAlgo = int(input("Choose sorting algorithm (1-#):"
+                     " \n 1. Selection Sort \n 2. Merge Sort "
+                     "\n 3. Insertion Sort \n 4. Bubble Sort \n "
+                     "5. Quick Sort -> "))
+
+time_init = time.perf_counter_ns()
 
 if sortAlgo == 1:
     selection_sort(need_sort, 0, 0)
-    print("Results:")
-    for i in need_sort:
-        print(i)
+
 
 # also print how fast it worked
 elif sortAlgo == 2:
     merge_sort(need_sort)
-    print("Results:")
-    for i in need_sort:
-        print(i)
+
 
 elif sortAlgo == 3:
     insertion_sort(need_sort)
-    print("Results:")
-    for i in need_sort:
-        print(i)
+
 
 elif sortAlgo == 4:
     bubble_sort(need_sort)
-    print("Results:")
-    for i in need_sort:
-        print(i)
 
-elif (sortAlgo > 4):
-    print("coming soon!")
-    sortAlgo = int(input("Choose sorting algorithm (1-#): \n 1. Selection Sort \n 2. Merge Sort \n "
-                     "3. Insertion Sort \n 4. Bubble Sort \n-> "))
+
+elif sortAlgo == 5:
+    quick_sort(0, len(need_sort) - 1, need_sort)
+
+time_end = time.perf_counter_ns()
+
+print('Results: {}'.format(time_end - time_init))
+
+
+
