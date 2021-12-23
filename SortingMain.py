4f194d43e65d0bfb445a19583f7e7b7383a5c620
eltonv <eltonmvillalta@gmail.com>
2021-12-22 21:55:59 -0800

    -Added Merge Sort

--- SortingMain.py
+++ SortingMain.py
@@ -11,6 +11,40 @@ def selection_sort(to_sort, sorted_ind, min_ind):
         to_sort[j], to_sort[min_ind] = to_sort[min_ind], to_sort[j]
 
 
+def merge_sort(to_sort):
+    if len(to_sort) > 1:
+        mid = len(to_sort) // 2
+        first_half = to_sort[:mid]
+        second_half = to_sort[mid:]
+        merge_sort(first_half)
+        merge_sort(second_half)
+
+        i = j = k = 0
+
+        while i < len(first_half) and j < len(second_half):
+            if first_half[i] <= second_half[j]:
+                # The value from the left half has been used
+                to_sort[k] = first_half[i]
+                # Increase iterator
+                i += 1
+            else:
+                to_sort[k] = second_half[j]
+                j += 1
+            # Move to the next index
+            k += 1
+
+        # Remaining values
+        while i < len(first_half):
+            to_sort[k] = first_half[i]
+            i += 1
+            k += 1
+
+        while j < len(second_half):
+            to_sort[k] = second_half[j]
+            j += 1
+            k += 1
+
+
 # main
 number_elem = int(input("Enter the amount of numbers to sort -> "))
 if number_elem == 0:
@@ -23,7 +57,7 @@ for i in range(0, number_elem):
     element = int(input())
     need_sort.append(element)
 
-sortAlgo = int(input("Choose sorting algorithm (1-#): \n 1. Selection Sort \n -> "))
+sortAlgo = int(input("Choose sorting algorithm (1-#): \n 1. Selection Sort \n 2. Merge Sort \n -> "))
 
 if (sortAlgo == 1):
     selection_sort(need_sort, 0, 0)
@@ -32,7 +66,12 @@ if (sortAlgo == 1):
         print(i)
 
 # also print how fast it worked
+elif (sortAlgo == 2):
+    merge_sort(need_sort)
+    print("Results:")
+    for i in need_sort:
+        print(i)
 
-elif (sortAlgo > 1):
+elif (sortAlgo > 2):
     print("coming soon!")
     sortAlgo = int(input("Choose sorting algorithm (1-#): \n 1. Selection Sort \n -> "))
